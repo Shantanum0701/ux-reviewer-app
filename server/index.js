8649bc6989ab5da3e2af71ae60c0ef5d0e08a021
Shantanum0701 <marikshantanu@gmail.com>
2026-02-19 00:58:42 +0530

    Configure server to host frontend files

--- server/index.js
+++ server/index.js
@@ -112,6 +112,24 @@ async function processAudit(auditId, url) {
     }
 }
 
+
+// --- SERVE REACT FRONTEND ---
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the React app (client/dist)
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+// ----------------------------
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
